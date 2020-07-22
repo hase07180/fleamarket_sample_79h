a6007d43af3d74bc7109942b449b6f00148dd598
yuki464 <orenomusuko555@gmail.com>
2020-07-22 12:10:20 +0900

    祖先おk

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_003806) do
+ActiveRecord::Schema.define(version: 2020_07_22_011616) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
 
 end
