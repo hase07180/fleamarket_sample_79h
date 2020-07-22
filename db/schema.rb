c6ae27bc48ad01664151e4f53497d11ac24c8b65
yuki464 <orenomusuko555@gmail.com>
2020-07-22 10:45:30 +0900

    db保留

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,4 @@
 
 ActiveRecord::Schema.define(version: 2020_07_22_003806) do
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
