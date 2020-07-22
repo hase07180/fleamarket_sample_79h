ca456eee14372749a5d19ae3bbadc4292c993480
yuki464 <orenomusuko555@gmail.com>
2020-07-22 14:36:12 +0900

    マイグレートできました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_011616) do
+ActiveRecord::Schema.define(version: 2020_07_22_030217) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
